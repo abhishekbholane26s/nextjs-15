514e69a4259e403e7c0e8eac26cabfaebe2e5274
abhishekbholane26s <abhishekbholane26s@gmail.com>
2025-09-22 08:10:17 +0530

    shadcn-ui-theme-setupa-and-dark-theme-added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/theme/toggle";
 
+function HomeBtn() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      {/* <HomeBtn></HomeBtn> */}
+      <ModeToggle></ModeToggle>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
